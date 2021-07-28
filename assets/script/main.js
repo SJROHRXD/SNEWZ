915bc75e5f360b979871ee053aafd1556cf8c641
Ray Ashir <rayeed.ashir@gmail.com>
2021-07-27 21:57:08 -0700

    fix bug where company name and data displayed twice

--- assets/script/main.js
+++ assets/script/main.js
@@ -91,18 +91,19 @@ function callNewsApi(companySymbol) {
     input: stockData - json of the result from the api call
     return: none
 */
+$CompInfoBox = $(`<div class="box has-background-info has-text-white"></div>`)
 function processStockPriceResults(stockData) {
     console.log("stockData", stockData);
 
 
-    $CompInfoBox = $(`<div class="box has-background-info has-text-white"></div>`)
+    
     $companyName = $(`<h2 class="title has-text-white" id="companyName">Company Name: ${stockData['01. symbol']}</h2>`)
     $price = $(`<li class='is-small' id="price">Price: ${stockData['05. price']}</li>`)
     $openingPrice = $(`<li class='is-small' id="openingPrice">Opening Price: ${stockData['02. open']}</li>`)
     $weekHigh = $(`<li class='is-small' id="weekHigh">52 Week High: ${stockData['03. high']}</li>`)
     $volume = $(`<li class='is-small' id="volume">Volume: ${stockData['06. volume']}</li>`)
 
-
+    $CompInfoBox.empty();
     $($CompInfoBox).append($companyName, $price, $openingPrice, $weekHigh, $volume)
     $('#CompanyInfo').prepend($CompInfoBox)
 
